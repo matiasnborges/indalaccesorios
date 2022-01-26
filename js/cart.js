24b69f04fa8842b8289ee2c0835620d4eac16906
Borges Matias N <matias.borges@trp.com.ar>
2022-01-25 23:47:10 -0300

    Desafio9 - Eventos_GenerarHTML

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,6 @@
 //Declaracion de variables --------------------------------------------------------------------------------------------------------------------------------
 let condicionIva = 0;
+let nombreCliente = "";
 const iva = 21;
 let opcionElegida = 0;
 let subtotal = 0;
@@ -30,6 +31,7 @@ class Producto{
 productos.push(new Producto(1,"T-92 x 100unidades",1000));
 productos.push(new Producto(2,"T-87 x100 unidades",300));
 productos.push(new Producto(3,"H-62 Falleba",500));
+productos.push(new Producto(4,"T-88 x100 unidades",600));
 
 class ProductoComprado {
     constructor (codigo, descripcionComprada, cantidadComprada, subtotal) {
@@ -41,6 +43,27 @@ class ProductoComprado {
 }
 
 //Funciones --------------------------------------------------------------------------------------------------------------------------------
+function ingresarCliente() {
+    let clientDetails = document.getElementById("clientDetail");
+    clientDetails.addEventListener("submit", validarFormularioCliente);
+}
+
+function validarFormularioCliente(e){
+    let clientDetails = document.getElementById("clientDetail");
+    e.preventDefault();
+    console.log("Formulario Enviado");
+    condicionIva = document.getElementById("condicionIva").value;
+    nombreCliente = document.getElementById("nombre").value;
+    console.log(condicionIva);
+    clientDetails.parentNode.removeChild(clientDetails);
+    const saludo = document.getElementById("welcome");
+    let agregarSaludo = document.createElement("h2");
+    principalFunction(condicionIva);
+    agregarSaludo.innerHTML = `Bienvenido ${nombreCliente}, este es el detalle de tu compra: </h2>`;
+    saludo.appendChild(agregarSaludo);
+    
+}
+
 
 function elegirOpcion () {
     opcionElegida = parseInt(prompt(`Podes comprar los siguientes productos (precios sin IVA):
@@ -111,9 +134,14 @@ function ordenarProductos(ordenamiento) {
 
 // Algoritmo principal ---------------------------------------------------------------------------------------------------------------------
 
-condicionIva = parseInt(prompt(`Hola! Para comprar ingresa tu condicion de IVA: \n 1 - Responsable Inscripto \n 2 - Monotributista`));
-ordenamiento = parseInt(prompt(`Por favor elegi el orden en que queres ver los productos: \n 1 - Por codigo \n 2 - Por menor precio \n Si ingresas cualquier otra opcion se ordenara por codigo`));
-ordenarProductos(ordenamiento);
+
+//ordenamiento = parseInt(prompt(`Por favor elegi el orden en que queres ver los productos: \n 1 - Por codigo \n 2 - Por menor precio \n Si ingresas cualquier otra opcion se ordenara por codigo`));
+//ordenarProductos(ordenamiento);
+
+ingresarCliente();
+
+function principalFunction(condicionIva) {
+    
 
 if (condicionIva==1) {
 //Responsable Inscripto
@@ -133,6 +161,14 @@ if (condicionIva==1) {
         elegirOpcion();
     }
 
+    const tableTitle = document.getElementById("tableHead");
+    let titleProducts = document.createElement("tr");
+    titleProducts.innerHTML = ` <th scope="col">Cantidad</th>
+                                <th scope="col">Descripcion</th>
+                                <th scope="col">Subtotal</th>
+                                </tr>`;
+    tableTitle.appendChild(titleProducts);
+
     const tableElement = document.getElementById("productosComprados");
     productosComprados.forEach((producto) => {
         let productComprado = document.createElement("tr");
@@ -140,8 +176,7 @@ if (condicionIva==1) {
                 <td>${producto.cantidadComprada}</td>
                 <td>${producto.descripcionComprada}</td>
                 <td>$ ${producto.subtotal}</td>
-            </tr>
-            `;
+            </tr>`;
         tableElement.appendChild(productComprado);
     });
 
@@ -171,8 +206,14 @@ else if (condicionIva==2) {
     elegirOpcion();
     }
 
-    //alert(`El total de la compra es $${total} IVA incluido por ser monotributo`);
-    
+    const tableTitle = document.getElementById("tableHead");
+    let titleProducts = document.createElement("tr");
+    titleProducts.innerHTML = ` <th scope="col">Cantidad</th>
+                                <th scope="col">Descripcion</th>
+                                <th scope="col">Subtotal</th>
+                                </tr>`;
+    tableTitle.appendChild(titleProducts);
+   
     const tableElement = document.getElementById("productosComprados");
     productosComprados.forEach((producto) => {
         let productComprado = document.createElement("tr");
@@ -194,4 +235,6 @@ else if (condicionIva==2) {
 }
 else {
     alert("Ingresaste una opcion incorrecta, por favor volve a intentar ingresando: \n 1 - Para responsable inscripto \n 2 - Para monotributista");
+}
+
 }
